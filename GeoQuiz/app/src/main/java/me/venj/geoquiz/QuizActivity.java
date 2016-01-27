cc196b94209ad74e1c3e9f17406921e99f724330
Venj Chu <ersaclarke@gmail.com>
2016-01-27 10:28:07 +0800

    Example 1.

--- GeoQuiz/app/src/main/java/me/venj/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/me/venj/geoquiz/QuizActivity.java
@@ -9,12 +9,41 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.true_toast;
+        } else {
+            messageResId = R.string.false_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +62,21 @@ public class QuizActivity extends AppCompatActivity {
 
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener((view) -> {
-            Toast.makeText(QuizActivity.this, R.string.true_toast, Toast.LENGTH_SHORT).show();
+            checkAnswer(true);
         });
         mFalseButton = (Button)findViewById(R.id.false_button);
         mFalseButton.setOnClickListener((view) -> {
-            Toast.makeText(QuizActivity.this, R.string.false_toast, Toast.LENGTH_SHORT).show();
+            checkAnswer(false);
         });
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 
+        updateQuestion();
 
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener((view) -> {
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+            updateQuestion();
+        });
     }
 
     @Override
