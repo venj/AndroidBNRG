40fccec2c9707b091ac1344993574438e2c1bfea
Venj <ersaclarke@gmail.com>
2016-03-02 16:11:48 +0800

    Chapter 6 challenge

--- GeoQuiz/app/src/main/java/me/venj/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/me/venj/geoquiz/CheatActivity.java
@@ -1,7 +1,10 @@
 package me.venj.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,6 +24,7 @@ public class CheatActivity extends AppCompatActivity {
     private static final String TAG = "CHEAT_ACTIVITY";
 
     private TextView answerTextView;
+    private TextView apiLevelTextView;
     private Button showAnswerButton;
     private boolean answerIsTrue;
     private boolean isAnswerShown;
@@ -70,7 +75,30 @@ public class CheatActivity extends AppCompatActivity {
             answerTextView.setText(answerIsTrue ? R.string.true_button : R.string.false_button);
             isAnswerShown = true;
             setAnswerShownResult();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                int cx = showAnswerButton.getWidth() / 2;
+                int cy = showAnswerButton.getHeight() / 2;
+                float radius = showAnswerButton.getWidth() / 2;
+                Animator anim = ViewAnimationUtils.createCircularReveal(showAnswerButton, cx, cy, radius, 0);
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        answerTextView.setVisibility(View.VISIBLE);
+                        showAnswerButton.setVisibility(View.INVISIBLE);
+                    }
+                });
+                anim.start();
+            }
+            else {
+                answerTextView.setVisibility(View.VISIBLE);
+                showAnswerButton.setVisibility(View.INVISIBLE);
+            }
         });
+
+        apiLevelTextView = (TextView)findViewById(R.id.api_level_text_view);
+        apiLevelTextView.setText(String.format(getResources().getString(R.string.api_level_text), Build.VERSION.SDK_INT));
     }
 
     @Override
